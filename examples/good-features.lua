237bc41e1a86452f3bc30715b54f2b47b730fff5
Marco Scoffier <github@metm.org>
2012-02-02 22:40:18 -0500

    good features example.  unstable need to lookinto sba error

--- examples/good-features.lua
+++ examples/good-features.lua
@@ -9,8 +9,12 @@ p = xlua.Profiler()
 p:start('loading images')
 frames={}
 frames[1] = image.load('frame-000025.png')
-frames[2] = image.load('frame-000026.png')
-frames[3] = image.load('frame-000027.png')
+frames[2] = image.load('frame-000029.png')
+-- frames[3] = image.load('frame-000027.png')
+--frames[4] = image.load('frame-000031.png')
+--frames[5] = image.load('frame-000033.png')
+width = frames[1]:size(3)
+height = frames[1]:size(2)
 p:lap('loading images')
 
 p:start('get features to track')
@@ -38,13 +42,20 @@ for i = 2,nframes do
    diff:pow(2)
    cdiff = diff:select(2,1) + diff:select(2,2)
    for j = 1,cdiff:size(1) do 
-      if (math.floor(cdiff[j]) > 1) then 
+      if ((math.floor(cdiff[j]) > 1) or
+          (trackPoints[j][1] < 1) or
+          (trackPoints[j][2] < 1) or
+          (trackPoints[j][1] > width) or
+          (trackPoints[j][2] > height))
+      then 
          cFlag[j] = 0
       end
    end
    p:lap('backtrack features')
    trackPointsP = trackPoints
 end
+
+p:start('prep clean points')
 cIndex = 1
 for i = 1,nPoints do 
    if (cFlag[i] == 1) then 
@@ -55,6 +66,7 @@ end
 cIndex = cIndex-1
 cleanPoints = cleanPoints:narrow(1,1,cIndex)
 print('found '..cIndex..' good points')
+p:lap('prep clean points')
 
 p:start('sparse bundle adjustment')
 motion, structure, calibration = sfm.sba_points(cleanPoints)
@@ -62,15 +74,16 @@ p:lap('sparse bundle adjustment')
 
 p:start('interpolate tracking')
 flowfield = flowfield or 
-   torch.Tensor(1, frames[3]:size(2), frames[3]:size(3))
+   torch.Tensor(1, frames[nframes]:size(2), frames[nframes]:size(3))
 depth = structure:narrow(2,2,1)
-opencv.smoothVoronoi(cleanPoints:select(2,3),depth, flowfield)
+opencv.smoothVoronoi(cleanPoints:select(2,nframes), depth, flowfield)
 p:lap('interpolate tracking')
 
 p:start('display')
-dispimg = frames[3]:clone()
-opencv.drawFlowlinesOnImage{pair={cleanPoints:select(2,3),
-                                  cleanPoints:select(2,2)}, image=dispimg}
+dispimg = frames[nframes]:clone()
+opencv.drawFlowlinesOnImage{pair={cleanPoints:select(2,nframes),
+                                  cleanPoints:select(2,nframes-1)}, 
+                            image=dispimg}
 dispimg:div(dispimg:max())
 
 win = image.display{image={dispimg},win=win}
